889c33eac55c2a1dfaeafa095e22722205ff5e19
Nick Easton <nicholaseaston2021@u.northwestern.edu>
2021-01-25 15:54:22 -0600

    added comments and comment details endpoints

--- views/comments.py
+++ views/comments.py
@@ -1,12 +1,55 @@
-import models
 from flask import Response, request
-from flask import Blueprint
+from flask_restful import Resource
+from mongoengine import DoesNotExist, Q
+import models
+import json
 
-api = Blueprint('comments', __name__)
-@api.route('/api/comments/<post_id>/', methods=['GET', 'POST'])
-def get_comments(post_id):
-    if request.method == 'GET':
-        data = models.Comment.objects(post=post_id).to_json()
-        return Response(data, mimetype="application/json", status=200)
-    else:
-        return Response('POST method not handled yet.', mimetype="application/text", status=200)
+class CommentListEndpoint(Resource):
+
+    def queryset_to_serialized_list(self, queryset):
+        serialized_list = [
+            item.to_dict() for item in queryset
+        ]
+        return serialized_list
+    
+    def get(self):
+        data = models.Comment.objects
+        data = self.queryset_to_serialized_list(data)
+        return Response(json.dumps([data]), mimetype="application/json", status=200)
+
+    def post(self):
+        body = request.get_json()
+        comment = models.Comment(**body).save()
+        serialized_data = {
+            'id': str(comment.id),
+            'message': 'Comment {0} successfully created.'.format(comment.id)
+        }
+        return Response(json.dumps(serialized_data), mimetype="application/json", status=201)
+        
+class CommentDetailEndpoint(Resource):
+    def put(self, id):
+        comment = models.Comment.objects.get(id=id)
+        request_data = request.get_json()
+        comment.author = request_data.get('author')
+        comment.content = request_data.get('content')
+        comment.save()
+        print(comment.to_json())
+        return Response(comment.to_json(), mimetype="application/json", status=200)
+    
+    def delete(self, id):
+        comment = models.Comment.objects.get(id=id)
+        comment.delete()
+        serialized_data = {
+            'message': 'Comment {} deleted.'.format(id)
+        }
+        return Response(json.dumps([serialized_data]), mimetype="application/json", status=200)
+
+    def get(self, id):
+        comment = models.Comment.objects.get(id=id)
+        return Response(comment.to_json(), mimetype="application/json", status=200)
+
+def initialize_routes(api):
+    api.add_resource(CommentListEndpoint, '/api/comments', '/api/comments/')
+    api.add_resource(CommentDetailEndpoint, '/api/comments/<id>', '/api/comments/<id>/')
+    # api.add_resource(CommentListEndpoint, '/api/posts/<post_id>/comments', '/api/posts/<post_id>/comments/')
+    # api.add_resource(CommentDetailEndpoint, '/api/posts/<post_id>/comments/<id>', '/api/posts/<post_id>/comments/<id>/')
